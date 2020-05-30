a62298afcbfd9b255659e7e1c7358d39b0ada80d
seveniruby <seveniruby@gmail.com>
2020-05-30 16:00:29 +0800

    add search delete

--- src/test/java/test_web/wework/page/ContactPage.java
+++ src/test/java/test_web/wework/page/ContactPage.java
@@ -9,6 +9,7 @@ import java.time.Duration;
 public class ContactPage {
     By addMember=By.linkText("添加成员");
     By username=By.name("username");
+    By delete=By.linkText("删除");
 
     public ContactPage addMember(String username, String acctid, String mobile) {
         //todo:
@@ -29,10 +30,25 @@ public class ContactPage {
             MainPage.driver.findElement(addMember).click();
         }
 
-        MainPage.driver.findElement(this.username).sendKeys("4");
-        MainPage.driver.findElement(By.name("acctid")).sendKeys("4");
-        MainPage.driver.findElement(By.name("mobile")).sendKeys("15600534764");
+        MainPage.driver.findElement(this.username).sendKeys(username);
+        MainPage.driver.findElement(By.name("acctid")).sendKeys(acctid);
+        MainPage.driver.findElement(By.name("mobile")).sendKeys(mobile);
         MainPage.driver.findElement(By.cssSelector(".js_btn_save")).click();
         return this;
+    }
+
+    public ContactPage search(String keyword){
+        MainPage.driver.findElement(By.id("memberSearchInput")).sendKeys(keyword);
+        new WebDriverWait(MainPage.driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(delete));
+        return this;
+    }
+
+    public ContactPage delete(){
+        MainPage.driver.findElement(delete).click();
+        MainPage.driver.findElement(By.linkText("确认")).click();
+        MainPage.driver.findElement(By.id("clearMemberSearchInput")).click();
+        return this;
+
     }
 }
--- src/test/java/test_web/wework/testcase/TestContact.java
+++ src/test/java/test_web/wework/testcase/TestContact.java
@@ -1,16 +1,29 @@
 package test_web.wework.testcase;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import test_web.wework.page.ContactPage;
 import test_web.wework.page.MainPage;
 
 public class TestContact {
+    static MainPage main;
+    static ContactPage contact;
+    @BeforeAll
+    static void beforeAll(){
+        main=new MainPage();
+        contact=main.toContact();
+    }
     @Test
     void testAddMember(){
-        MainPage main=new MainPage();
-        main.toContact().addMember("3", "3", "15600534763");
-        //todo: assert
 
+        contact.addMember("3", "3", "15600534763");
+        //todo: assert
+    }
+
+    @Test
+    void testSearch(){
+        contact.search("3").delete();
     }
 
     @AfterAll
