6c3ff108f48956677c1ebc09f33f1ecb62a001a2
seveniruby <seveniruby@gmail.com>
2020-07-21 02:04:34 +0800

    小程序代码更新

--- src/test/java/test_app/webview/WeiXinTest.java
+++ src/test/java/test_app/webview/WeiXinTest.java
@@ -26,12 +26,35 @@ import java.util.concurrent.TimeUnit;
 public class WeiXinTest {
 
     private AndroidDriver<WebElement> driver;
+    private int index = 0;
 
     @Before
     public void setUp() throws MalformedURLException {
-
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("deviceName", "测试人社区 ceshiren.com");
+        desiredCapabilities.setCapability("appPackage", "com.tencent.mm");
+        desiredCapabilities.setCapability("appActivity", "com.tencent.mm.ui.LauncherUI");
+        desiredCapabilities.setCapability("unicodeKeyboard", "true");
+        desiredCapabilities.setCapability("resetKeyboard", "true");
+        //高危操作，如果设置错误，聊天记录会被清空，建议使用小号测试
+        desiredCapabilities.setCapability("noReset", "true");
 
+        //第一步：设置正确的chromedriver
+        //简单粗暴的解决方案
+        desiredCapabilities.setCapability("chromedriverExecutable",
+                "/Users/seveniruby/projects/chromedriver/chromedrivers/chromedriver_78.0.3904.11");
+//      desiredCapabilities.setCapability("chromedriverExecutable",
+//        "/Users/seveniruby/projects/chromedriver/chromedrivers/chromedriver_2.23");
+        //完善的版本选择方案，不过会优先找android webview默认实现
+//        desiredCapabilities.setCapability("chromedriverExecutableDir",
+//                "/Users/seveniruby/projects/chromedriver/chromedrivers");
+//        desiredCapabilities.setCapability("chromedriverChromeMappingFile",
+//                "/Users/seveniruby/projects/Java3/src/main/resources/mapping.json");
+        //打印更多chromedriver的log方便定位问题
+        desiredCapabilities.setCapability("showChromedriverLog", true);
+
+        //第二步：设置chromeoption传递给chromedriver
         //因为小程序的进程名跟包名不一样，需要加上这个参数
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setExperimentalOption("androidProcess", "com.tencent.mm:appbrand0");
@@ -39,33 +62,15 @@ public class WeiXinTest {
         //必须得加上，因为默认生成browserName=chrome的设置，需要去掉
         desiredCapabilities.setCapability("browserName", "");
 
-        desiredCapabilities.setCapability("platformName", "android");
-        desiredCapabilities.setCapability("deviceName", "测试人社区 ceshiren.com");
-        desiredCapabilities.setCapability("appPackage", "com.tencent.mm");
-        desiredCapabilities.setCapability("appActivity", "com.tencent.mm.ui.LauncherUI");
-        desiredCapabilities.setCapability("unicodeKeyboard", "true");
-        desiredCapabilities.setCapability("resetKeyboard", "true");
-
-        //高危操作，如果设置错误，聊天记录会被清空，建议使用小号测试
-        desiredCapabilities.setCapability("noReset", "true");
+        //第三步：设置adb proxy
         //通过自己的adb代理修复chromedriver的bug并解决@xweb_devtools_remote的问题
         desiredCapabilities.setCapability("adbPort", "5038");
+
         //加速
         desiredCapabilities.setCapability("skipLogcatCapture", "true");
         //用于快速测试
 //        desiredCapabilities.setCapability("dontStopAppOnReset", "true");
 
-
-//        简单粗暴的解决方案
-        desiredCapabilities.setCapability("chromedriverExecutable", "/Users/seveniruby/projects/chromedriver/chromedrivers/chromedriver_78.0.3904.11");
-//        desiredCapabilities.setCapability("chromedriverExecutable", "/Users/seveniruby/projects/chromedriver/chromedrivers/chromedriver_2.23");
-
-
-        //完善的版本选择方案，不过会优先找android webview默认实现
-//        desiredCapabilities.setCapability("chromedriverExecutableDir", "/Users/seveniruby/projects/chromedriver/chromedrivers");
-//        desiredCapabilities.setCapability("chromedriverChromeMappingFile", "/Users/seveniruby/projects/Java3/src/main/resources/mapping.json");
-        //打印更多chromedriver的log方便定位问题
-        desiredCapabilities.setCapability("showChromedriverLog", true);
 
         URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
 
@@ -91,6 +96,7 @@ public class WeiXinTest {
         driver.findElement(By.className("android.widget.EditText")).click();
         driver.findElement(By.xpath("//*[@text='取消']"));
         driver.findElement(By.className("android.widget.EditText")).sendKeys("雪球");
+        screenshot();
         driver.findElement(By.className("android.widget.Button")).click();
         driver.findElement(By.xpath("//*[@text='自选']"));
 
@@ -106,11 +112,13 @@ public class WeiXinTest {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         findTopWindow();
+        screenshot();
         driver.findElement(By.cssSelector("[src*=stock_add]")).click();
 //        driver.findElement(By.cssSelector("[src=\"/static/img/stock_add@3x.png\"]")).click();
 
         new WebDriverWait(driver, 10).until((driver) -> driver.getWindowHandles().size() > 2);
         findTopWindow();
+        screenshot();
         WebElement input = driver.findElement(By.cssSelector("._input"));
         //html输入不生效
 //        input.sendKeys("xiaomi");
@@ -124,30 +132,27 @@ public class WeiXinTest {
         //native原生输入可以
         driver.context("NATIVE_APP");
         driver.getKeyboard().sendKeys("jd");
+        screenshot();
         driver.context("WEBVIEW_xweb");
-
         driver.findElement(By.cssSelector(".stock__item")).click();
-
         //截图
+        findTopWindow();
+        screenshot();
+    }
+
+    public void screenshot() {
+        //截图
+        index += 1;
+        String path = "/tmp";
         try {
-            FileUtils.copyFile(driver.getScreenshotAs(OutputType.FILE), new File("/tmp/1.png"));
+            FileUtils.copyFile(driver.getScreenshotAs(OutputType.FILE),
+                    new File(String.format("%s/wx_%s.png", path, index)));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
     public void findTopWindow() {
-//        driver.getWindowHandles().stream().reduce((a, b)->{
-//            if(driver.getTitle().contains(":VISIBLE")){
-//                return a;
-//            }else{
-//                driver.switchTo().window(b);
-//                return b;
-//            }
-//        });
-
         for (String win : driver.getWindowHandles()) {
             if (driver.getTitle().contains(":VISIBLE")) {
                 System.out.println(driver.getTitle());
@@ -162,7 +167,6 @@ public class WeiXinTest {
 
     @After
     public void tearDown() throws InterruptedException {
-        Thread.sleep(10000);
         driver.quit();
     }
 }
